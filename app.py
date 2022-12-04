a1f7ac6d1cb681cbd1c13439f35afba25d0e08ba
Cesar Arnouk <arnoukcesar@gmail.com>
2022-12-03 19:32:33 -0500

    routes and api hook done

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, jsonify, request
 from model.user import User
 from model.watchlist import Watchlist
 from model.stock import Stock
+from stock_fetcher import get_stock_price
 
 app = Flask(__name__)
 
 user_hashmap = {}
+
+@app.route("/stock/<ticker>")
+def get_stock(ticker):
+    stock = get_stock_price(ticker)
+    return jsonify(stock.__dict__)
 
 @app.route("/get_watchlist", methods=["GET"])
 def get_watchlist():
@@ -19,8 +25,8 @@ def get_watchlist():
     return jsonify(stocks_to_list(tmp_user.get_watchlist().get_stocks()))
 
 
-@app.route("/add_to_watchlist", methods=["PUT"])
-def add_to_watchlist():
+@app.route("/add_to_watchlist/<ticker>", methods=["PUT"])
+def add_to_watchlist(ticker):
     user_IP = request.remote_addr
     tmp_user = None
     if user_IP in user_hashmap:
@@ -28,13 +34,13 @@ def add_to_watchlist():
     else:
         tmp_user = User(user_IP)
         user_hashmap[user_IP] = tmp_user
-    stock = Stock("Apple", "AAPL", 100)
+    stock = get_stock_price(ticker)
     tmp_user.add_to_watchlist(stock)
     return jsonify(stocks_to_list(tmp_user.get_watchlist().get_stocks()))
 
 
-@app.route("/remove_from_watchlist", methods=["PUT"])
-def remove_from_watchlist():
+@app.route("/remove_from_watchlist/<ticker>", methods=["PUT"])
+def remove_from_watchlist(ticker):
     user_IP = request.remote_addr
     tmp_user = None
     if user_IP in user_hashmap:
@@ -42,7 +48,7 @@ def remove_from_watchlist():
     else:
         tmp_user = User(user_IP)
         user_hashmap[user_IP] = tmp_user
-    stock = Stock("Apple", "AAPL", 100)
+    stock = get_stock_price(ticker)
     tmp_user.remove_from_watchlist(stock)
     return jsonify(stocks_to_list(tmp_user.get_watchlist().get_stocks()))
 

a3f6e4d0dff0be05b940b8a7c5c8e0f0923ca850
Cesar Arnouk <dev.cesararnouk@gmail.com>
2022-12-05 13:31:31 -0500

    testing CI/CD

--- test/integration/restAPI_tests.py
+++ test/integration/restAPI_tests.py
@@ -14,26 +14,29 @@ class TestRestAPI(unittest.TestCase):
         stock = get_stock_price(ticker)
         response = requests.get(url + "stock/" + ticker)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), stock.__dict__)
+        self.assertEqual(response.json()["ticker"], stock.ticker)
+        self.assertEqual(response.json()["name"], stock.name)
         ticker = "GOOG"
         stock = get_stock_price(ticker)
         response = requests.get(url + "stock/" + ticker)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), stock.__dict__)
+        self.assertEqual(response.json()["ticker"], stock.ticker)
+        self.assertEqual(response.json()["name"], stock.name)
         ticker = "MSFT"
         stock = get_stock_price(ticker)
         response = requests.get(url + "stock/" + ticker)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), stock.__dict__)
+        self.assertEqual(response.json()["ticker"], stock.ticker)
+        self.assertEqual(response.json()["name"], stock.name)
     
     def test_get_watchlist(self):
-        tmp_user = User("127.0.0.1")
+        tmp_user = User("127.0.0.5")
         response = requests.get(url + "get_watchlist")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), stocks_to_list(tmp_user.get_watchlist().get_stocks()))
         
     def test_add_to_watchlist(self):
-        tmp_user = User("127.0.0.1")
+        tmp_user = User("127.0.0.5")
         ticker = "AAPL"
         stock = get_stock_price(ticker)
         tmp_user.add_to_watchlist(stock)
@@ -42,7 +45,7 @@ class TestRestAPI(unittest.TestCase):
         self.assertEqual(response.json(), stocks_to_list(tmp_user.get_watchlist().get_stocks()))
 
     def test_remove_from_watchlist(self):
-        tmp_user = User("127.0.0.1")
+        tmp_user = User("127.0.0.5")
         ticker = "AAPL"
         stock = get_stock_price(ticker)
         tmp_user.remove_from_watchlist(stock)
